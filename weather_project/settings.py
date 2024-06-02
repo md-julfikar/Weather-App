656290d588cc6a3465f448896d3931d8082163f6
julfikar <julfikarjim69@gmail.com>
2024-06-03 05:09:51 +0600

    Everything OK

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -67,10 +67,14 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'weather', 'static')
-# ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'weather', 'static')
+]
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'weather', 'static','image')
+
 
 
 DEBUG = True
