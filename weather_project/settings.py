25f8292480aaccc6af26e993a2644e30ff5aa1aa
julfikar <julfikarjim69@gmail.com>
2024-06-03 04:20:38 +0600

    k

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -43,12 +43,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'weather_project.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -73,10 +67,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'weather', 'static')
-]
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'weather', 'static')
+# ]
 
 
 DEBUG = True
