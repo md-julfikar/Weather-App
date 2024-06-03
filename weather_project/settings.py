a60b864465c8cce25c1c877ecf903ea770fd854c
julfikar <julfikarjim69@gmail.com>
2024-06-03 06:30:45 +0600

    i

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -1,4 +1,5 @@
 import os
+from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -9,7 +10,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'weather', 
+    'weather',
 ]
 
 MIDDLEWARE = [
@@ -35,14 +36,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.static', 
+                'django.template.context_processors.static',
             ],
         },
     },
 ]
 
 WSGI_APPLICATION = 'weather_project.wsgi.application'
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -58,7 +58,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-from django.core.management.utils import get_random_secret_key
 
 SECRET_KEY = get_random_secret_key()
 LANGUAGE_CODE = 'en-us'
@@ -68,8 +67,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'weather', 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'weather', 'static')]
+
 DEBUG = True
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
